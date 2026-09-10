08f9a833771fdcad6547a35c614bf68ef8009c2c
yt <yeeteng041101@gmail.com>
2026-09-10 13:57:44 +0800

    ss

---
+++ .widget_preview/lib/src/dtd/dtd_connection_info.dart
@@ -0,0 +1,9 @@
+// ignore_for_file: implementation_imports
+
+const String kWidgetPreviewDtdUri = 'ws://127.0.0.1:60303/9kmmoeYpmPQ=';
+const String kWidgetPreviewService =
+    'widget-preview-bde4e0ba-a753-42ca-a622-654b4c575ac3';
+const String kWidgetPreviewScaffoldStream =
+    'WidgetPreviewScaffold-bde4e0ba-a753-42ca-a622-654b4c575ac3';
+const String kProjectRootPath =
+    r'C:\Users\yeete\Desktop\Mobile Assignment\industryhub';
